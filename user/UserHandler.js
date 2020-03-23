87210d2868cf2d0612a9488847655021ded0f75f
vvanand007 <coolvvanand@gmail.com>
2020-03-23 20:51:44 +0530

    register and login API up

---
+++ user/UserHandler.js
@@ -0,0 +1,23 @@
+const connectToDatabase = require("../db");
+const User = require("./User");
+
+module.exports.getUsers = (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  return connectToDatabase()
+    .then(getUsers)
+    .then(users => ({
+      statusCode: 200,
+      body: JSON.stringify(users)
+    }))
+    .catch(err => ({
+      statusCode: err.statusCode || 500,
+      headers: { "Content-Type": "text/plain" },
+      body: JSON.stringify({ message: err.message })
+    }));
+};
+
+function getUsers() {
+  return User.find({})
+    .then(users => users)
+    .catch(err => Promise.reject(new Error(err)));
+}
